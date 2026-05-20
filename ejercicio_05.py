c9f710fe2c93c1a19e1bf805325712e15c5be038
Bautista Molina <b.molina938@itsv.edu.ar>
2026-05-20 21:36:08 +0000

    Resuelve ejercicio 5

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -11,4 +11,4 @@ def filtrar_pares_B(numeros):
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 print(f"Versión A: {filtrar_pares_A(numeros)}")
-print(f"Versión B: {filtrar_pares_B(numeros)}")
+print(f"Versión B: {filtrar_pares_B(numeros)}") 
