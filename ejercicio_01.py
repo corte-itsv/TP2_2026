6a8c7f407e5236471843c81cffdfa242ffa432c0
Benjamin <b.pagliettini937@gmail.com>
2026-05-20 18:30:59 +0000

    Ejercicio 1

---
+++ ejercicio_01.py
@@ -0,0 +1,17 @@
+nota = int(input("Ingrese la nota del alumno: ")) 
+
+def clasificar_nota(nota):
+    if nota == 10:
+        return "Perfecto"
+    elif nota == 8 or nota == 9:
+        return "Muy bueno"
+    elif nota == 6 or nota == 7:
+        return "Aprobado"
+    elif nota == 4 or nota == 5:
+        return "Desaprobado (cerca)"
+    elif nota == 1 or nota == 2 or nota == 3:
+        return "Desaprobado (lejos)"
+    else:
+        return "Nota inválida"
+resultado = clasificar_nota(nota)
+print(resultado)
