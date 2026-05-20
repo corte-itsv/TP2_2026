26b2c1c2f2ba73a9481a644d631d40f7ce8db45a
Franco Ozan <f.ozan481@itsv.edu.ar>
2026-05-20 14:32:54 -0300

    Agrega ejercicio 03

---
+++ ejercicio_03.py
@@ -0,0 +1,19 @@
+def contar_aprobados(notas):
+    aprobados = 0
+    for nota in notas:
+        if nota >= 6:
+            aprobados += 1
+    return aprobados
+
+def contar_desaprobados(notas):
+    desaprobados = 0
+    for nota in notas:
+        if nota < 6:
+            desaprobados += 1
+    return desaprobados
+
+notas = [8, 3, 6, 10, 4, 7, 5, 9, 6, 2]
+
+print(f"Total: {len(notas)} alumnos")
+print(f"Aprobados: {contar_aprobados(notas)}")
+print(f"Desaprobados: {contar_desaprobados(notas)}")
