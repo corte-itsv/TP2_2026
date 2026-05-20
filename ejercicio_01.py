8383f0eccfc30df56629e11717f00fd23b0565b6
Eitan <e.landenberg543@itsv.edu.ar>
2026-05-20 16:24:10 +0000

    Resuelve ejercicio 1

---
+++ ejercicio_01.py
@@ -0,0 +1,17 @@
+def clasificar_nota(nota):
+    if nota == 10:
+        print("Excelente")
+    elif nota in [9, 8]:
+        print("Muy bueno")
+    elif nota in [7, 6]:
+        print("aprobado")
+    elif nota in [5, 4]:
+        print("desaprobado (cerca)")
+    elif nota in [3, 2, 1]:
+        print("desaprobado (lejos)")
+    else:
+        print("nota invalida")
+nota = int(input("Introducí tu nota: "))
+clasificar_nota(nota)
+
+    
