f8230cbf5ae2265a3b78d97fa730adf5c4998197
Benjamín Oller <b.oller.padilla320@itsv.edu.ar>
2026-05-20 17:26:18 +0000

    Agrega ejercicio 1

---
+++ ejercicio_01.py
@@ -0,0 +1,19 @@
+def clasificar_nota(nota):
+    if nota == 10:
+        return "Perfecto"
+    elif nota in [8, 9]:
+        return "Muy Bueno"
+    elif nota in [6, 7]:
+        return "Aprobado"
+    elif nota in [5, 4]:
+        return "Desaprobado (cerca)"
+    elif nota in [3, 2, 1]:
+        return "Desaprobado (lejos)"
+    else:
+        return "Nota Invalida"
+
+notas_prueba  = [10, 7, 4, 0, 11]
+
+for n in notas_prueba:
+    resultado = clasificar_nota(n)
+    print(f"{n} -> {resultado}")
