bb3fd0c63ec026e3a924b0a2c277167305f4115d
Eitan <e.landenberg543@itsv.edu.ar>
2026-05-20 17:05:06 +0000

    Resuelve ejercicio 3

---
+++ ejercicio_03.py
@@ -0,0 +1,18 @@
+def contar_aprobados(notas):
+    aprobados = 0
+    for nota in notas: 
+        if nota >= 6:
+            aprobados = aprobados + 1
+    return aprobados
+
+def contar_desaprobados(notas):
+    desaprobados = 0
+    for nota in notas: 
+        if nota < 6:
+            desaprobados = desaprobados + 1
+    return desaprobados
+notas = [8, 3, 6, 10, 4, 7, 5, 9, 6, 2]
+cant_aprobados = contar_aprobados(notas)
+print(f"Cantidad de aprobados: {cant_aprobados}")
+cant_desaprobados = contar_desaprobados(notas)
+print(f"Cantidad de aprobados: {cant_desaprobados}")
