1d2f99d47f71319592bac4105e92ce4fc967364d
Bautista Molina <b.molina938@itsv.edu.ar>
2026-05-20 21:02:21 +0000

    Sexto commit

---
+++ ejercicio_06.py
@@ -0,0 +1,10 @@
+def invertir(lista):
+    lista_invertida = []
+    for i in range(len(lista) - 1, -1, -1):
+        elemento = lista[i]    
+        lista_invertida.append(elemento)
+    return lista_invertida  
+original = [1, 2, 3, 4, 5]
+print(invertir(original)) 
+letras = ["a", "b", "c", "d"]
+print(invertir(letras))
