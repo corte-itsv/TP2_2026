9e4903d975bc738723a17af6837c6dfc8ef46187
Thiago Prados <t.prados.kantor299@gmail.com>
2026-05-20 18:07:03 +0000

    agrega ejercicio 1

---
+++ ejercicio_01.py
@@ -0,0 +1,18 @@
+print("Escribe la nota:")
+nota = int(input())
+
+def clasificar(nota): 
+    if nota == 10 :
+        return "Perfecto"
+    elif nota == 9 or nota == 8 :
+        return "Muy bueno"
+    elif nota == 7 or nota == 6:
+        return "Aprobado"
+    elif nota == 5 or nota == 4:
+        return "Desaprobado (cerca)"
+    elif nota == 3 or nota == 2 or nota == 1:
+        return "Desaprobado (lejos)"
+    else :
+        return "nota invalida"
+
+print(clasificar(nota))
