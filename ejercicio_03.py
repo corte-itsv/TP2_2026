cf0ffc5716f23c5c5428fec614369f87fb911a2c
bauti <b.cabrera.pozzobon861@itsv.edu.ar>
2026-05-20 18:11:45 +0000

    ejercicio_03.py

---
+++ ejercicio_03.py
@@ -0,0 +1,19 @@
+def contar_aprobados(notas):
+    cantidad = 0
+    for nota in notas:
+        if nota >= 6:
+            cantidad += 1
+    return cantidad
+def contar_desaprobados(notas):
+    cantidad = 0
+    for nota in notas:
+        if nota < 6:
+            cantidad += 1
+    return cantidad
+
+notas = [8, 3, 6, 10, 4, 7, 5, 9, 6, 2]
+
+print("Total:", len(notas), "alumnos")
+print("Aprobados:", contar_aprobados(notas), "alumnos aprobados")
+print("Desaprobados:", contar_desaprobados(notas), "alumnos desaprobados")
+
