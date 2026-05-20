787196810e13451d3e43fc12205fb342fb156427
Benjamín Oller <b.oller.padilla320@itsv.edu.ar>
2026-05-20 18:31:36 +0000

    Agrega ejercicio 2

---
+++ ejercicio_02.py
@@ -0,0 +1,16 @@
+def calcular_promedio(notas):
+    if len(notas) == 0: 
+        return 0
+
+    promedio =  sum(notas) / len(notas) 
+
+    return (promedio)
+
+
+lista_a = [8, 9, 7, 10, 6]
+lista_b = [4, 5, 3, 6, 4, 5]
+lista_c = []
+
+print(f"Promedio A:", calcular_promedio(lista_a))
+print(f"Promedio B:", calcular_promedio(lista_b))
+print(f"Promedio C:", calcular_promedio(lista_c))
