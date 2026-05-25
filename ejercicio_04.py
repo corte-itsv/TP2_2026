c8aec523d4fa1612785563bc245de53e255a920b
Daniel Zilberberg <d.zilberberg.mirman250@itsv.edu.ar>
2026-05-25 00:21:26 +0000

    corrige ejercicio 04

--- ejercicio_04.py
+++ ejercicio_04.py
@@ -8,6 +8,8 @@ def min_max(numeros):
         if numero > maximo:
          maximo=numero
     return minimo, maximo
+
+
 numeros = [34, 7, 89, 12, 56, 3, 78, 45]
 minimo, maximo = min_max(numeros)
 print(f"Minimo: {minimo}")
