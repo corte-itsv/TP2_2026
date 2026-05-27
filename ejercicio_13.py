25e2feed1f7860a4716d8a9b918c998b13ee92d7
Eitan <e.landenberg543@itsv.edu.ar>
2026-05-27 20:35:47 +0000

    Solo imprime el nombre del mejor alumno

--- ejercicio_13.py
+++ ejercicio_13.py
@@ -1,21 +1,18 @@
 def calcular_promedios(curso):
-    # ¡Esto estuvo impecable!
     promedios = {nombre: sum(ns) / len(ns) for nombre, ns in curso.items()}
     return promedios
 
 
 def alumno_destacado(promedios):
     mejor_alumno = ""
-    max_promedio = 0  # Empezamos en cero para comparar números reales
+    max_promedio = 0  
     
-    # Usamos .items() para obtener el nombre y el promedio directo
     for nombre, promedio in promedios.items():
         if promedio > max_promedio:
             max_promedio = promedio
             mejor_alumno = nombre
             
-    # Retornamos una tupla con el nombre y el número para armar el print pedido
-    return mejor_alumno, max_promedio
+    return mejor_alumno
 
 
 def alumnos_aprobados(promedios):
@@ -37,7 +34,8 @@ curso = {
 dicc_promedios = calcular_promedios(curso)
 print(f"Promedios: {dicc_promedios}")
 
-destacado, nota_max = alumno_destacado(dicc_promedios)
+destacado = alumno_destacado(dicc_promedios)
+nota_max = dicc_promedios[destacado]
 print(f"Alumno destacado: {destacado} ({nota_max})")
 
 lista_aprobados = alumnos_aprobados(dicc_promedios)
