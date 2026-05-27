3a75f38f81fa93d9e94acef0f4a330e4c762a204
Felipe Vivas <f.vivas.lucero547@itsv.edu.ar>
2026-05-27 17:01:14 +0000

    ejercicio05

---
+++ ejercicio_05.py
@@ -0,0 +1,17 @@
+def filtrar_pares_A(numeros):
+    pares = []
+    for numero in numeros:
+        if numero % 2 == 0:
+            pares.append(numero)
+    return pares
+
+def filtrar_pares_B(numeros):
+    return [numero for numero in numeros if numero % 2 == 0] 
+
+def filtrar_pares(numeros):
+    return filtrar_pares_B(numeros)
+
+if __name__ == "__main__":
+    numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    print(f"Versión A: {filtrar_pares_A(numeros)}")
+    print(f"Versión B: {filtrar_pares_B(numeros)}")
