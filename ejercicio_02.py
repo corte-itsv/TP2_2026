ccfb2c06e715e9ed2c59753e63c76476e8c374b6
Franco Ozan <f.ozan481@itsv.edu.ar>
2026-05-27 15:43:01 -0300

    Modifica ejercicio 02

--- ejercicio_02.py
+++ ejercicio_02.py
@@ -2,13 +2,12 @@ def calcular_promedio(notas):
 
     if len(notas) == 0:
         return 0
-    return sum(notas) / len(notas)
+    promedio = sum(notas) / len(notas)
     return round(promedio, 2)
 
 lista_a = [8, 9, 7, 10, 6]
 
 lista_b = [4, 5, 3, 6, 4, 5]
-
 lista_c = []
 
 
