1a9664fd29d0492116ca12dcc99f551eafbad3d9
Jeremias Purro <j.purro119@itsv.edu.ar>
2026-05-27 17:07:55 +0000

    Agrega validacion ejercicio 7

---
+++ ejercicio_07.py
@@ -0,0 +1,21 @@
+
+def esta_en_lista(lista, elemento):
+    for e in lista:
+        if e == elemento:
+            return True
+    return False
+
+
+def posicion_en_lista(lista, elemento):
+    for i in range(len(lista)):
+        if lista[i] == elemento:
+            return i
+    return -1
+
+frutas = ["manzana", "banana", "pera", "uva", "kiwi"]
+
+print("¿'pera' está en la lista?", esta_en_lista(frutas, "pera"))
+print("¿'mango' está en la lista?", esta_en_lista(frutas, "mango"))
+
+print("Posición de 'uva':", posicion_en_lista(frutas, "uva"))
+print("Posición de 'mango':", posicion_en_lista(frutas, "mango"))
