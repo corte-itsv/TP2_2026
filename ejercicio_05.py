31f58535e7d6e2e5c7b5772f5ff6da280cb27928
maximovartanian795 <m.vartanian795@itsv.edu.ar>
2026-05-27 18:53:55 +0000

    corrige ej_05

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -1,16 +1,16 @@
-def filtrar_pares_A(numeros):
+def filtrar_pares(numeros):
     pares = []  
     for numero in numeros:
         if numero % 2 == 0:
             pares.append(numero)  
     return pares
 
-def filtrar_pares_B(numeros):
+def filtrar_pares(numeros):
     return [numero for numero in numeros if numero % 2 == 0] 
 
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 
-print(f"Versión A: {filtrar_pares_A(numeros)}")
-print(f"Versión B: {filtrar_pares_B(numeros)}")
+print(f"Versión A: {filtrar_pares(numeros)}")
+print(f"Versión B: {filtrar_pares(numeros)}")
     
