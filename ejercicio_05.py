e98083129c75f5a61bfd231b5609a163bb12eb9d
Agustin Martinez <a.martinez777@itsv.edu.ar>
2026-05-27 16:00:24 -0300

    ejercicio 05 bien hecho

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -1,15 +1,9 @@
-def filtrar_pares_a(numeros):
-    pares = []
-    for num in numeros:
-        if num % 2 == 0:
-            pares.append(num)
-    return pares
+def filtrar_pares(numeros):
+    resultado = []
+    for n in numeros:
+        if n % 2 == 0:
+            resultado.append(n)
+    return resultado
 
-def filtrar_pares_b(numeros):
-    return [num for num in numeros if num % 2 == 0]
-
-if __name__ == "__main__":
-    numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    
-    print(f"Versión A: {filtrar_pares_a(numeros)}")
-    print(f"Versión B: {filtrar_pares_b(numeros)}")
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+print(filtrar_pares(numeros))
