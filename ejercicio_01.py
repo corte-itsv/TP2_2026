214507451906eba9e0f96695d355de549b08cf09
Santino Parraga <s.parraga208@itsv.edu.ar>
2026-05-27 17:17:30 +0000

    Agrega ejercicio 1

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -1,4 +1,4 @@
-def clasificar_notas(nota):
+def clasificar_nota(nota):
     if nota == 10:
         return("Perfecto")
     elif nota == 9 or nota == 8:
@@ -11,7 +11,6 @@ def clasificar_notas(nota):
         return("Desaprobado (lejos)")
     else:
         return("Nota inválda")
-clasificar_notas
 
 for nota in [10, 7, 4, 0, 11]:
-    print(f"{nota} = {clasificar_notas(nota)}")
+    print(f"{nota} = {clasificar_nota(nota)}")
