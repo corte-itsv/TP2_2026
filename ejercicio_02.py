c5c9af3a1f604f7c26a5cdda9bfa75530dd83a20
Franco Ozan <f.ozan481@itsv.edu.ar>
2026-05-27 15:39:06 -0300

    Modifica ejercicio 02

--- ejercicio_02.py
+++ ejercicio_02.py
@@ -1,10 +1,17 @@
 def calcular_promedio(notas):
+
     if len(notas) == 0:
         return 0
     return sum(notas) / len(notas)
+    return round(promedio, 2)
+
 lista_a = [8, 9, 7, 10, 6]
+
 lista_b = [4, 5, 3, 6, 4, 5]
+
 lista_c = []
+
+
 print(f"Promedio A: {calcular_promedio(lista_a)}")
 print(f"Promedio B: {calcular_promedio(lista_b)}")
 print(f"Promedio C: {calcular_promedio(lista_c)}")
