fdb722870bb2263a9468cda2dbdc77519bf74e27
maximovartanian795 <m.vartanian795@itsv.edu.ar>
2026-05-27 18:52:07 +0000

    corrige errores de ortografia

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -2,15 +2,15 @@ def clasificar_nota(nota):
     if nota==10:
         return "Perfecto"
     elif nota == 8 or nota == 9:
-        return "Muy Bueno"
+        return "Muy bueno"
     elif nota == 7 or nota == 6:
-        return "aprobado"
+        return "Aprobado"
     elif nota == 5 or nota == 4:
-        return "Desaprobado (Cerca)"
+        return "Desaprobado (cerca)"
     elif nota == 3 or nota == 2 or nota == 1:
-        return "Desaprobado (Lejos)"
+        return "Desaprobado (lejos)"
     else:
-        return "Nota invalida"
+        return "Nota inválida"
 print("10 →" , clasificar_nota(10))
 print("7 →" , clasificar_nota(7))
 print("4 →" , clasificar_nota(4))
