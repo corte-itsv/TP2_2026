853d3da0933ed891f3fb4dc487f2c80fc97ecefa
sarce7411 <s.arce940@itsv.edu.ar>
2026-05-27 14:19:34 +0000

    hago ej 5

---
+++ ejercicio_05.py
@@ -0,0 +1,12 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+par = numeros[0]
+
+def filtrar_pares(numeros):
+    pares=[]
+    for numero in numeros:
+        if numero % 2 == 0:
+            pares.append(numero)
+    return pares
+
+print(filtrar_pares(numeros))
