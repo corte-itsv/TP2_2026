42c164012212a42f7862085a0b764dabc9800f70
bruno aguirre <b.aguirre759@itsv.edu.ar>
2026-05-27 13:13:01 -0300

    ejercicio 2

---
+++ ejercicio_2.py
@@ -0,0 +1,17 @@
+def calcular_promedio(notas):
+    if len(notas) == 0:
+        return 0
+    
+    promedio = sum(notas) / len(notas)
+    return round(promedio, 2)
+
+
+# Listas
+lista_a = [8, 9, 7, 10, 6]
+lista_b = [4, 5, 3, 6, 4, 5]
+lista_c = []
+
+# Pruebas
+print("Promedio A:", calcular_promedio(lista_a))
+print("Promedio B:", calcular_promedio(lista_b))
+print("Promedio C:", calcular_promedio(lista_c))
