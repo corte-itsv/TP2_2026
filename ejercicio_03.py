37a04ff693f394dd67d83957762a59cf546a9ebd
sarce7411 <s.arce940@itsv.edu.ar>
2026-05-27 19:00:44 +0000

    bufix 3 1

--- ejercicio_03.py
+++ ejercicio_03.py
@@ -3,7 +3,7 @@ notas = [8, 3, 6, 10, 4, 7, 5, 9, 6, 2]
 def contar_aprobados(notas):
     aprobados = 0
     for nota in notas:
-        if nota >= 6:
+        if round(nota) >= 6:
             aprobados = aprobados + 1
     return aprobados
 
@@ -11,12 +11,13 @@ def contar_aprobados(notas):
 def contar_desaprobados(notas):
     desaprobados = 0
     for nota in notas:
-        if nota <= 5:
+        if round(nota) <= 5:
             desaprobados = desaprobados + 1
     return desaprobados
 
 cant_aprobados = contar_aprobados(notas)
 cant_desaprobados = contar_desaprobados(notas)
 
-print(f"La cantidad de aprobados es {cant_aprobados}")
-print(f"La cantidad de desaprobados es {cant_desaprobados}")
+print(f"Total: {len(notas)}")
+print(f"Aprobados: {cant_aprobados}")
+print(f"Desaprobados: {cant_desaprobados}")
