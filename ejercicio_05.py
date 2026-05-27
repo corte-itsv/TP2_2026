9e4696d4b7a2d3f363253cc9c0a5e07850077c76
Lucca Garcia <l.garcia.269@itsv.edu.ar>
2026-05-27 18:38:16 +0000

    Agrega ejercicio 5

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -1,14 +1,21 @@
-def filtrar_pares_A(numeros):
-    pares = []  
+def filtrar_pares_for(numeros):
+    pares = []
+
     for numero in numeros:
         if numero % 2 == 0:
-            pares.append(numero)  
+            pares.append(numero)
+
     return pares
 
-def filtrar_pares_B(numeros):
-    return [numero for numero in numeros if numero % 2 == 0] 
+
+def filtrar_pares_comprehension(numeros):
+    return [numero for numero in numeros if numero % 2 == 0]
+
 
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-print(f"Versión A: {filtrar_pares_A(numeros)}")
-print(f"Versión B: {filtrar_pares_B(numeros)}")
+version_a = filtrar_pares_for(numeros)
+version_b = filtrar_pares_comprehension(numeros)
+
+print("Versión A:", version_a)
+print("Versión B:", version_b)
