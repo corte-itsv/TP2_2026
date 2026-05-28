e31459c526b2a6312ee5a139ee45085282a342a5
maximovartanian795 <m.vartanian795@itsv.edu.ar>
2026-05-28 22:43:47 +0000

    corrige ejercicio_03

--- ejercicio_03.py
+++ ejercicio_03.py
@@ -1,21 +1,20 @@
 def contar_aprobados(notas):
-    notas = [8, 3, 6, 10, 4, 7, 5, 9, 6, 2]
-    aprobados = 0
+    cantidad = 0
     for nota in notas:
         if nota >= 6:
-            aprobados += 1
-    return aprobados
+            cantidad += 1
+    return cantidad
+
 
 def contar_desaprobados(notas):
-    notas = [8, 3, 6, 10, 4, 7, 5, 9, 6, 2]
-    desaprobados = 0
+    cantidad = 0
     for nota in notas:
         if nota < 6:
-            desaprobados += 1
-    return desaprobados
-alumnos = 10 
+            cantidad += 1
+    return cantidad
 
+notas = [8, 3, 6, 10, 4, 7, 5, 9, 6, 2]
 
-print("alumnos:", alumnos)
-print("aprobados:", contar_aprobados([8, 3, 6, 10, 4, 7, 5, 9, 6, 2]))
-print("desaprobados:", contar_desaprobados([8, 3, 6, 10, 4, 7, 5, 9, 6, 2]))
+print("Total:", len(notas), "alumnos")
+print("Aprobados:", contar_aprobados(notas), "alumnos ")
+print("Desaprobados:", contar_desaprobados(notas), "alumnos")
