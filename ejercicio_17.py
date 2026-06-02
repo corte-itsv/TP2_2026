dd1e771b62b77445835f8b11ccd92fe24184b816
sanchez <s.sanchez.chanquia155@itsv.edu.ar>
2026-06-02 22:20:11 +0000

    Resuelve ejercicio 17

---
+++ ejercicio_17.py
@@ -0,0 +1,20 @@
+def cifrar(texto, desplazamiento):
+    resultado = ""
+    for letra in texto:
+        if letra.isalpha():
+            nueva = chr((ord(letra) - ord('a') + desplazamiento) % 26 + ord('a'))
+            resultado += nueva
+        else:
+            resultado += letra
+    return resultado
+ 
+def descifrar(texto_cifrado, desplazamiento):
+    return cifrar(texto_cifrado, -desplazamiento)
+ 
+mensaje = "hola mundo"
+clave   = 3
+cifrado = cifrar(mensaje, clave)
+ 
+print("\nOriginal:   " + mensaje)
+print("Cifrado:    " + cifrado)
+print("Descifrado: " + descifrar(cifrado, clave))
