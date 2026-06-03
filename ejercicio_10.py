ea2eae49c4fb58dd7b14b6468390611469fb911a
Lucca Garcia <l.garcia.269@itsv.edu.ar>
2026-06-03 17:13:11 +0000

    Correccion ejercicio_10

--- ejercicio_10.py
+++ ejercicio_10.py
@@ -1,14 +1,10 @@
 def tabla_multiplicar(numero):
-    tabla = []
-    for i in range(1, 11):
-        tabla.append((i, numero * i))
-    return tabla
+    return [(i, i * numero) for i in range(1, 11)]
 
 def mostrar_tabla(tabla, numero):
-    print("=== Tabla del", numero, "===")
-    for i, resultado in tabla:
-        
-        print(numero, "x", i, "=", resultado)
+    print(f"=== Tabla del {numero} ===")
+    for mult, res in tabla:
+        print(f"{numero} x {mult:>2} ={res:>3}")
 
 numero = 7
 t = tabla_multiplicar(numero)
