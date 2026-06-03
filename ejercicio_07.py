361a5d2d983c807e7524348247cae7a24e5c6604
fausto <f.monserrat015@itsv.edu.ar>
2026-06-03 17:22:20 +0000

    Agrego ejercicio 7

---
+++ ejercicio_07.py
@@ -0,0 +1,18 @@
+
+def esta_en_lista(lista, elemento):
+    for fruta in lista:
+        if fruta == elemento:
+            return True  
+    return False       
+
+def posicion_en_lista(lista, elemento):
+    for i in range(len(lista)):
+        if lista[i] == elemento:
+            return i     
+    return -1            
+
+frutas = ["manzana", "banana", "pera", "uva", "kiwi"]
+
+print("¿'pera' está en la lista?", esta_en_lista(frutas, "pera"))
+print("¿'mango' está en la lista?", esta_en_lista(frutas, "mango"))
+print("Posición de 'uva':", posicion_en_lista(frutas, "uva"))
