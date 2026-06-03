6975eae39806f025b1f90f6ed204d012da64c120
Juan Bertossi <j.bertossi680@itsv.edu.ar>
2026-06-03 16:17:48 +0000

    Modifica ejercicio 8

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -1,16 +1,12 @@
-#version 1
-def pares_1(numeros):
+def filtrar_pares(numeros):
     pares = []
     for numero in numeros:
         if numero % 2 == 0:
             pares.append(numero)
     return pares
 
-#version 2
-def pares_2(numeros):
-    return [numero for numero in numeros if numero % 2 == 0]
+lista_numeros = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+numeros_pares = filtrar_pares(lista_numeros)
 
-print("Versión A:", pares_1(numeros))
-print("Versión B:", pares_2(numeros))
+print(numeros_pares)
