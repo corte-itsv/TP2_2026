0cba5f3c93d1ef9a91de785d8dfcd0ed6bab1353
sanchez <s.sanchez.chanquia155@itsv.edu.ar>
2026-06-03 17:27:05 +0000

    Modificacion creo que por tercera vez del ejercicio 8

--- ejercicio_08.py
+++ ejercicio_08.py
@@ -1,35 +1,24 @@
 def contar_frecuencia(palabras):
     frecuencias = {}
-    
     for palabra in palabras:
         if palabra in frecuencias:
             frecuencias[palabra] += 1
         else:
             frecuencias[palabra] = 1
-    
     return frecuencias
 
-
 def palabra_mas_repetida(frecuencias):
-
-    palabra_max = None
-    frecuencia_max = 0
-    
-    for palabra, frecuencia in frecuencias.items():
-        if frecuencia > frecuencia_max:
-            frecuencia_max = frecuencia
-            palabra_max = palabra
-    
-    return palabra_max, frecuencia_max
-
-
+    mas_repetida = None
+    for palabra in frecuencias:
+        if mas_repetida is None or frecuencias[palabra] > frecuencias[mas_repetida]:
+            mas_repetida = palabra
+    return mas_repetida
 
 palabras = ["python", "es", "genial", "python", "es", "facil", "python"]
 
 frecuencias = contar_frecuencia(palabras)
 print(frecuencias)
 
-palabra, veces = palabra_mas_repetida(frecuencias)
-print(f"La palabra más repetida es: '{palabra}' ({veces} veces)") 
-
+mas_repetida = palabra_mas_repetida(frecuencias)
+print(f"La palabra más repetida es: '{mas_repetida}' ({frecuencias[mas_repetida]} veces)")
  #6767676767
