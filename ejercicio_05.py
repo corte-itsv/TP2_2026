cdda0d644b7bd2096e5bf041bf4ab1e87ca0c0d2
Lucas Andino <l.andino568@itsv.edu.ar>
2026-06-03 17:37:51 +0000

    ejercicio_05.py

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -1,17 +1,13 @@
 def filtrar_pares(numeros):
-    numeros_paresA = [n for n in numeros if n % 2 == 0]
-    numeros_paresB = []
-    for n in numeros:
-        if n % 2 == 0:
-            numeros_paresB.append(n)
-    return numeros_paresA, numeros_paresB
+    numeros_pares = [n for n in numeros if n % 2 == 0]
+    return numeros_pares
 
 
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 
-numeros_paresA, numeros_paresB = filtrar_pares(numeros)
+numeros_pares, numeros_pares = filtrar_pares(numeros)
 
 
-print("Versión A:", numeros_paresA)
-print("Versión B:", numeros_paresB)
+print("Versión A:", numeros_pares)
+print("Versión B:", numeros_pares)
