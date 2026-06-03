18e29a53ffb702ba3af285f0e9def95bdba93e67
Geremias Sahonero <g.sahonero470@itsv.edu.ar>
2026-06-03 18:32:04 +0000

    agrega ejercicio 8

---
+++ ejercicio_08.py
@@ -0,0 +1,24 @@
+def contar_frecuencia(palabras):
+    frecuencias = {}
+    for palabra in palabras:
+        if palabra in frecuencias:
+            frecuencias[palabra] += 1
+        else:
+            frecuencias[palabra] = 1
+    return frecuencias
+
+def palabra_mas_repetida(frecuencias):
+    max_palabra = None
+    max_cantidad = -1
+    for palabra, cantidad in frecuencias.items():
+        if cantidad > max_cantidad:
+            max_cantidad = cantidad
+            max_palabra = palabra
+    return max_palabra
+
+palabras = ["python", "es", "genial", "python", "es", "facil", "python"]
+frecuencias_dict = contar_frecuencia(palabras)
+print(frecuencias_dict)
+top_palabra = palabra_mas_repetida(frecuencias_dict)
+top_cantidad = frecuencias_dict[top_palabra]
+print(f"La palabra más repetida es: '{top_palabra}' ({top_cantidad} veces)")
