f3ec834ff1db9e45d447f46f75234f08b774f098
Lisandro <l.cabrera406@itsv.edu.ar>
2026-06-03 14:22:35 -0300

    Corrige ejercicio 5

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -6,8 +6,13 @@ def filtrar_pares_A(numeros):
             
     return pares
 
+
 def filtrar_pares_B(numeros):
     return [numero for numero in numeros if numero % 2 == 0]
+
+
+def filtrar_pares(numeros):
+    return filtrar_pares_B(numeros)
 
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
