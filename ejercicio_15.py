36a9a7b9d9dbe0b25c862d4ad3a6cc5f17a6a494
ciro <c.gioda158@itsv.edu.ar>
2026-06-03 00:59:15 +0000

    ejerccio_15.py

---
+++ ejercicio_15.py
@@ -0,0 +1,23 @@
+def agrupar_por_inicial(nombres):
+    grupos = {}
+
+    for nombre in nombres:
+        inicial = nombre[0]
+
+        if inicial not in grupos:
+            grupos[inicial] = []
+
+        grupos[inicial].append(nombre)
+
+    return grupos
+
+
+nombres = [
+    "Ana", "Alberto", "Belen", "Bruno", "Carlos",
+    "Camila", "Ana Paula", "Diego", "Daniela"
+]
+
+resultado = agrupar_por_inicial(nombres)
+
+for inicial, lista in resultado.items():
+    print(f"{inicial}: {lista}")
