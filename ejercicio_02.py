d827c36cf9f036d05e42193c7551ad6a97aa220c
Lucas Andino <l.andino568@itsv.edu.ar>
2026-06-03 17:36:08 +0000

    ejercicio_02.py

--- ejercicio_02.py
+++ ejercicio_02.py
@@ -1,12 +1,16 @@
 def calcular_promedio(notas):
     if len(notas) == 0:
         return 0
-    return sum(notas) / len(notas)
+    return round(sum(notas) / len(notas), 2)
+
 
 A = [8, 9, 7, 10, 6]
 B = [4, 5, 3, 6, 4, 5]
 C = []
+
+
 nombres = ["A", "B", "C"]
+
 
 for i, notas in enumerate([A, B, C]):
     promedio = calcular_promedio(notas)
