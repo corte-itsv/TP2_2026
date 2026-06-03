bf08d662e6d4cc40361ad387472a5184415df2ba
fausto <f.monserrat015@itsv.edu.ar>
2026-06-03 18:56:02 +0000

    Correccion del ejercicio 1

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -1,16 +1,16 @@
-def clasificar_notas(nota):
+def clasificar_nota(nota):
     if nota == 10:
-        return("Perfecto")
+        return "Perfecto"
     elif nota == 9 or nota == 8:
-        return("Muy bueno")
+        return "Muy bueno"
     elif nota == 7 or nota == 6:
-        return("Aprobado")
+        return "Aprobado"
     elif nota == 5 or nota == 4:
-        return("Desaprobado (cerca)")
+        return "Desaprobado (cerca)"
     elif nota == 1 or nota == 2 or nota == 3:
-        return("Desaprobado (lejos)")
+        return "Desaprobado (lejos)"
     else:
-        return("Nota inválda")
+        return "Nota inválida"
 
 for nota in [10, 7, 4, 0, 11]:
-    print(f"{nota} = {clasificar_notas(nota)}")
+    print(f"{nota} = {clasificar_nota(nota)}")
