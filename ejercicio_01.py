a18b3b5460c60659150a8235baad70c705ec5a06
francisco <f.guzman.comba828@itsv.edu.ar>
2026-06-03 17:24:03 +0000

    ejercicio_01.py

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -1,24 +1,20 @@
-def clasificar_nota(nota):   
-    
+def clasificar_nota(nota):
     if nota == 10:
-        return("Perfecto")
-    elif nota == 9 or nota == 8:
-        return("Muy bueno")
-    elif nota == 7 or nota == 6:
-        return("Aprobado")
-    elif nota == 5 or nota == 4:
-        return("Desaprobado (cerca)")
-    elif nota == 3 or nota == 2 or nota == 1:
-        return("Desaprobado (lejos)")
-    elif nota == 0:
-        return("Nota no Valida")
+        return "Perfecto"
+    elif nota in (8, 9):
+        return "Muy bueno"
+    elif nota in (6, 7):
+        return "Aprobado"
+    elif nota in (4, 5):
+        return "Desaprobado (cerca)"
+    elif nota in (1, 2, 3):
+        return "Desaprobado (lejos)"
     else:
-        return("Nota no válida")
+        return "Nota inválida"
 
-
-notas=[10, 7, 4, 0, 11]
-for n in notas:
-    resultado = clasificar_nota(n)
-    print(f"{n} -> {resultado}")
+# Pruebas
+notas_test = [10, 7, 4, 0, 11]
+for n in notas_test:
+    print(f"{n} → {clasificar_nota(n)}")
 
     
