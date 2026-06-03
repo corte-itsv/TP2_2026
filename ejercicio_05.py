3db06687308ec8bb68bcaee31832809e5adc3692
Tomas Alfaro <t.alfaro.arguello251@itsv.edu.ar>
2026-06-03 16:29:04 +0000

    agrega ejercicio 5

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -3,11 +3,10 @@ def filtrar_pares_a(numeros):
     for num in numeros:
         if num % 2 == 0:
             pares.append(num)
-    return pares
-
-def filtrar_pares_b(numeros):
-    return [num for num in numeros if num % 2 == 0]
+    return pares 
 
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+par = numeros[0] 
+
 print(f"Versión A: {filtrar_pares_a(numeros)}")
-print(f"Versión B: {filtrar_pares_b(numeros)}")
+print(f"Versión B: {filtrar_pares_a(numeros)}")
