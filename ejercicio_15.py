9ee0c91600392e94e2ae8e0e84ed0eaeaf0ad284
Lucca Garcia <l.garcia.269@itsv.edu.ar>
2026-06-03 17:29:32 +0000

    Agrega ejercicio_15

---
+++ ejercicio_15.py
@@ -0,0 +1,16 @@
+def agrupar_por_inicial(nombres):
+    iniciales = {}
+    for nombre in nombres:
+        inicial = nombre[0]
+        if inicial not in iniciales:
+            iniciales[inicial] = []
+        iniciales[inicial].append(nombre)
+    return iniciales
+
+nombres = [
+    "Ana", "Alberto", "Belen", "Bruno", "Carlos", "Camila", "Ana Paula", "Diego", "Daniela",
+]
+
+resultado = agrupar_por_inicial(nombres)
+for inicial, lista_nombres in resultado.items():
+    print(f"{inicial}: {lista_nombres}")
