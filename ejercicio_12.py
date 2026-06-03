0e70cc0514555691fc83f9c9206c7ca16d5eb938
Juan Bertossi <j.bertossi680@itsv.edu.ar>
2026-06-03 17:14:27 +0000

    Agrega ejercicio 12

---
+++ ejercicio_12.py
@@ -0,0 +1,15 @@
+def interseccion(lista_a, lista_b):
+    return list(set(lista_a) & set(lista_b))
+
+def union(lista_a, lista_b):
+    return list(set(lista_a) | set(lista_b))
+
+def solo_en_a(lista_a, lista_b):
+    return list(set(lista_a) - set(lista_b))
+
+clase_lunes    = ["Ana", "Luis", "Sol", "Marcos", "Julia"]
+clase_miercoles = ["Ana", "Sol", "Pedro", "Julia", "Tomás"]
+
+print(f"Asistieron ambos días:{sorted(interseccion(clase_lunes, clase_miercoles))}")
+print(f"Asistieron al menos un día:{sorted(union(clase_lunes, clase_miercoles))}")
+print(f"Solo el lunes:{sorted(solo_en_a(clase_lunes, clase_miercoles))}")
