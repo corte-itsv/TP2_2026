e4186e7e7f03b2d679f01baa79b1d1d7e99774e0
Lucas Andino <l.andino568@itsv.edu.ar>
2026-06-03 00:02:49 +0000

    ejercicio 4X

---
+++ ejercicio_04.py
@@ -0,0 +1,17 @@
+def min_max(numeros):
+    if not numeros:
+        return None, None 
+    numero_minimo = numeros[0]
+    numero_maximo = numeros[0]
+    for numero in numeros:
+        if numero < numero_minimo:
+            numero_minimo = numero
+        if numero > numero_maximo:
+            numero_maximo = numero
+    return numero_minimo, numero_maximo
+
+numeros = [34, 7, 89, 12, 56, 3, 78, 45]
+
+minimo, maximo = min_max(numeros)
+print(f"Mínimo: {minimo}")
+print(f"Máximo: {maximo}")
