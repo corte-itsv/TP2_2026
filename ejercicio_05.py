5f490aba07b6cd3464f38cd9f4949ec9c5112c33
Geremias Sahonero <g.sahonero470@itsv.edu.ar>
2026-06-03 18:12:25 +0000

    correccion ejercicio 5

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -1,14 +1,11 @@
-def filtrar_pares_A(numeros):
-    pares = []  
-    for numero in numeros:
-        if numero % 2 == 0:    
-            pares.append(numero)  
-    return pares
-
-def filtrar_pares_B(numeros):
-    return [numero for numero in numeros if numero % 2 == 0] 
+def filtrar_pares(numeros):
+    pares = []
+    for num in numeros:
+        if num % 2 == 0:
+            pares.append(num) 
+    return pares  
 
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-print(f"Versión A: {filtrar_pares_A(numeros)}")
-print(f"Versión B: {filtrar_pares_B(numeros)}") 
+print(f"Versión A: {filtrar_pares(numeros)}")
+print(f"Versión B: {filtrar_pares(numeros)}")
