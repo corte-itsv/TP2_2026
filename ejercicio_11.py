cfd4eb126802dc66de9b4959fb06f4aad64aa3b1
Agustin Oliva <a.oliva.marchetto277@itsv.edu.ar>
2026-06-03 18:07:40 +0000

    ejercicio_11.py

---
+++ ejercicio_11.py
@@ -0,0 +1,15 @@
+def sin_duplicados(lista):
+    
+    nueva_lista = []
+    vistos = set()
+
+    for elemento in lista:
+            if elemento not in vistos:
+                nueva_lista.append(elemento)
+                vistos.add(elemento)
+    return nueva_lista
+lista_num = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+nombres = ["Ana", "Luis", "Ana", "Sol", "Luis", "Marcos"]
+
+print(sin_duplicados(lista_num))
+print(sin_duplicados(nombres))
