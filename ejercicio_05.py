ff695f477b6f42396cab694546555469f4982859
joaco <j.madero313@itsv.edu.ar>
2026-06-03 17:06:44 +0000

    correccion ejercicio_05

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -1,15 +1,14 @@
-def pares_1(numeros):
-    pares = []
-    for numero in numeros:
-        if numero % 2 == 0:
-            pares.append(numero)
-    return pares
+def filtrar_pares(numeros):
+    lista_pares = []
 
-#version 2
-def pares_2(numeros):
-    return [numero for numero in numeros if numero % 2 == 0]
+    for numero in numeros:
+        if numero %2 == 0:
+            lista_pares.append(numero)
+    
+    return lista_pares
 
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-print("Versión A:", pares_1(numeros))
-print("Versión B:", pares_2(numeros))
+resultado = filtrar_pares(numeros)
+
+print(resultado)
