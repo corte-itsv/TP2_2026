74f0368e1a80bcbb678476b2e9a5928de782c092
Lucca Garcia <l.garcia.269@itsv.edu.ar>
2026-06-03 18:03:25 +0000

    Correccion ejercicio_09

--- ejercicio_09.py
+++ ejercicio_09.py
@@ -1,28 +1,36 @@
 def agregar_contacto(agenda, nombre, telefono):
     agenda[nombre] = telefono
-
 def buscar_contacto(agenda, nombre):
     if nombre in agenda:
         return agenda[nombre]
-    return "Contacto no encontrado"
+    else:
+        return "Contacto no encontrado"
+
 
 def eliminar_contacto(agenda, nombre):
     if nombre in agenda:
         del agenda[nombre]
 
+
+
 def mostrar_agenda(agenda):
-    print("=== AGENDA ===")
-    for n in sorted(agenda):
-        print(n, ":", agenda[n])
+    for nombre in sorted(agenda):
+        print(f"{nombre}: {agenda[nombre]}")
+
 
 agenda = {}
 agregar_contacto(agenda, "Ana", "351-1234")
 agregar_contacto(agenda, "Luis", "351-5678")
 agregar_contacto(agenda, "Marcos", "351-9012")
 
+print(f"=== AGENDA ===")
 mostrar_agenda(agenda)
 
-print("\nTeléfono de Luis:", buscar_contacto(agenda, "Luis"))
-print("Pedro:", buscar_contacto(agenda, "Pedro"))
+print()
+print(f"Teléfono de Luis: {buscar_contacto(agenda, "Luis")}")
+print(f"Teléfono de Pedro: {buscar_contacto(agenda, "Pedro")}")
 eliminar_contacto(agenda, "Marcos")
+print()
+
+print(f"=== AGENDA ===")
 mostrar_agenda(agenda)
