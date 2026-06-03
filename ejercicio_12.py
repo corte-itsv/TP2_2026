a9520990366cb9277d5001238ec9df05a094a339
joaco <j.madero313@itsv.edu.ar>
2026-06-03 18:10:52 +0000

    correccion_ejercicio12.py

--- ejercicio_12.py
+++ ejercicio_12.py
@@ -1,27 +1,21 @@
 def interseccion(lista_a, lista_b):
-    return set(lista_a) & set(lista_b)
-    set_a=set(lista_a)
-    set_b=set(lista_b)
-    return sorted(set_a & set_b)
-
-def union(lista_a, lista_b):
-    return set(lista_a) | set(lista_b)
-
-def diferencia(lista_a, lista_b):
-    return set(lista_a) - set(lista_b)
-    set_a=set(lista_a)
-    set_b=set(lista_b)
-    return sorted(set_a | set_b)
-    
+    return list(set(lista_b) & set(lista_a))
 
 def solo_en_a(lista_a, lista_b):
-    set_a=set(lista_a)
-    set_b=set(lista_b)
-    return sorted(set_a - set_b)
+    return list(set(lista_a) - set(lista_b))
+
+def union(lista_a, lista_b):
+    return list(set(lista_a) | set(lista_b))
 
 clase_lunes    = ["Ana", "Luis", "Sol", "Marcos", "Julia"]
 clase_miercoles = ["Ana", "Sol", "Pedro", "Julia", "Tomás"]
 
-print(f"Asistieron ambos días: {sorted(interseccion(clase_lunes, clase_miercoles))}")
-print(f"Asistieron al menos un día: {sorted(union(clase_lunes, clase_miercoles))}")
-print(f"Solo el lunes: {sorted(diferencia(clase_lunes, clase_miercoles))}")
+ambos = interseccion(clase_lunes, clase_miercoles)
+solo_a = solo_en_a(clase_lunes, clase_miercoles)
+unite = union(clase_lunes, clase_miercoles) 
+
+
+print(f"Asistieron ambos días: {sorted(ambos)}")
+print(f"Asistieron al menos un día: {sorted(unite)}")
+print(f"Solo el lunes: {sorted(solo_a)}")
+                   
