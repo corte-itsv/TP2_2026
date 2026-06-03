6e037d7f3afa4363d1de57991dd5ce559e19fbde
joaco <j.madero313@itsv.edu.ar>
2026-06-03 16:30:26 +0000

    corrige ejercicio_02

--- ejercicio_02.py
+++ ejercicio_02.py
@@ -1,9 +1,12 @@
 def calcular_promedio(notas):
     if len(notas) == 0:
-        return 0
+      return 0
+    
+    
+    promedio = sum (notas)/ len(notas)
+    
+    return (promedio)
 
-    promedio = sum(notas) / len(notas)
-    return round(promedio, 1)
 
 
 lista_a = [8, 9, 7, 10, 6]
