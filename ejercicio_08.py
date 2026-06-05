b99d315f6f973c7bfbbd8945e560922c96d0081d
fausto <f.monserrat015@itsv.edu.ar>
2026-06-05 22:29:19 +0000

    Correccion ejercicio 8

--- ejercicio_08.py
+++ ejercicio_08.py
@@ -1,20 +1,17 @@
 def contar_frecuencia(lista_palabras):
     frecuencias = {}
     for palabra in lista_palabras:
-     
         frecuencias[palabra] = frecuencias.get(palabra, 0) + 1
     return frecuencias
 
 def palabra_mas_repetida(frecuencias):
-
     palabra_max = max(frecuencias, key=frecuencias.get)
-    cantidad = frecuencias[palabra_max]
-    return palabra_max, cantidad
+    return palabra_max
 
 palabras = ["python", "es", "genial", "python", "es", "facil", "python"]
 
 frecuencias = contar_frecuencia(palabras)
-palabra, veces = palabra_mas_repetida(frecuencias)
+palabra = palabra_mas_repetida(frecuencias)
 
 print(frecuencias)
-print(f"La palabra más repetida es: '{palabra}' ({veces} veces)")
+print(f"La palabra más repetida es: '{palabra}'")
