cd1c0a0b3754f1c30cf6db8e725537d0ba15dc2a
Thiago Prados <t.prados.kantor299@gmail.com>
2026-06-07 13:48:06 +0000

    2da correccion ej_05

--- ejercicio_05.py
+++ ejercicio_05.py
@@ -1,9 +1,13 @@
-def filtrar_pares(numeros):
+def filtrar_pares_a(numeros):
     pares = []
     for numero in numeros:
         if numero %2 == 0 :
             pares.append(numero)
     return pares
 
+def filtrar_pares_b(numeros):
+    return [numero for numero in numeros if numero % 2 == 0]
+
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-print(filtrar_pares(numeros))
+print(f"Versión A: {filtrar_pares_a(numeros)}")
+print(f"Versión B: {filtrar_pares_b(numeros)}")
