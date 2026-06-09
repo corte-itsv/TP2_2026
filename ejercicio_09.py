c3d08d20697cc65db84276ca44f969a674afa34d
Lucas Andino <l.andino568@itsv.edu.ar>
2026-06-09 23:29:17 +0000

    ejercicio 09 pero devolviendo en orden alfabetica

--- ejercicio_09.py
+++ ejercicio_09.py
@@ -25,7 +25,7 @@ def mostrar_agenda(agenda):
     if len(agenda) == 0:
         return "La agenda está vacía"
     else:
-        contactos = [f"{nombre}: {telefono}" for nombre, telefono in agenda.items()]
+        contactos = [f"{nombre}: {telefono}" for nombre, telefono in sorted(agenda.items())]
         return "\n".join(contactos)
 
 
