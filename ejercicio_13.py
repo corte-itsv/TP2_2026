6f35a552e65e5de0b1a36a02c8c8de25b06bd694
Lucas Andino <l.andino568@itsv.edu.ar>
2026-06-09 23:09:17 +0000

    es el ejercicio 13

---
+++ ejercicio_13.py
@@ -0,0 +1,29 @@
+def calcular_promedios(curso):
+    promedios = {}
+    for alumno, notas in curso.items():
+        promedio = sum(notas) / len(notas)
+        promedios[alumno] = promedio
+    return promedios
+
+
+def alumno_destacado(promedios):
+    alumno_destacado = max(promedios, key=promedios.get)
+    return alumno_destacado 
+
+def alumnos_aprobados(promedios):
+    aprobados = [alumno for alumno, promedio in promedios.items() if promedio >= 6]
+    return aprobados
+
+
+curso = {
+    "Ana":     [9, 10, 8, 9],
+    "Luis":    [6,  5, 7, 6],
+    "Sol":     [10, 9, 10, 8],
+    "Marcos":  [4,  5, 3, 6],
+    "Julia":   [7,  8, 7, 9],
+}
+
+print(f"Promedios: {calcular_promedios(curso)})")
+print(f"Alumno destacado: {alumno_destacado(calcular_promedios(curso))}, ({calcular_promedios(curso)[alumno_destacado(calcular_promedios(curso))]})") 
+print(f"Aprobados: {alumnos_aprobados(calcular_promedios(curso))}")
+
