d10d278e79934885b316862bfffe05eb20506aff
Octavio Gambino <o.gambino250@itsv.edu.ar>
2026-06-10 02:16:44 +0000

    Resuelve ejercicio 7

---
+++ ejercicio_07.py
@@ -0,0 +1,17 @@
+def esta_en_lista(lista, elemento):
+    for item in lista:
+        if item == elemento:
+            return True
+    return False
+    
+def posicion_en_lista(lista, elemento):
+    for indice in range(len(lista)):
+        if lista[indice] == elemento:
+            return indice
+    return -1
+frutas = ["manzana", "banana", "pera", "uva", "kiwi"]
+
+print("¿'pera' está en la lista?", esta_en_lista(frutas, "pera"))
+print("¿'mango' está en la lista?", esta_en_lista(frutas, "mango"))
+print("Posición de 'uva':", posicion_en_lista(frutas, "uva"))
+print("Posición de 'mango':", posicion_en_lista(frutas, "mango"))
