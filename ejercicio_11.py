dc3241c07161536194569151cc153bda2cbe285f
Lucas Andino <l.andino568@itsv.edu.ar>
2026-06-10 01:20:49 +0000

    es el ejercicio 11

--- ejercicio_11.py
+++ ejercicio_11.py
@@ -5,8 +5,10 @@ def sin_duplicados(lista):
             resultado.append(elemento)
     return resultado
 
+
 lista = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 nombres = ["Ana", "Luis", "Ana", "Sol", "Luis", "Marcos"]
+
 
 print(sin_duplicados(lista))
 print(sin_duplicados(nombres))
