d144d78d304a2ec609849381ec2d2d281e633e00
Lucas Andino <l.andino568@itsv.edu.ar>
2026-06-10 01:34:46 +0000

    es el ejercicio 16

---
+++ ejercicio_16.py
@@ -0,0 +1,42 @@
+def promedio(notas):
+    return sum(notas) / len(notas)
+
+
+def condicion(promedio):
+    if promedio >= 6:
+        return "Aprobado"
+    else:
+        return "Desaprobado"
+
+def reporte_curso(curso):
+    reporte = {}
+    for alumno, notas in curso.items():
+        prom = promedio(notas)
+        reporte[alumno] = (prom, condicion(prom))
+    return reporte 
+
+
+def resumen(curso):
+    promedios = {alumno: promedio(notas) for alumno, notas in curso.items()}
+    alumno_destacado = max(promedios, key=promedios.get), 
+    promedio_destacado = promedios[alumno_destacado[0]]
+    aprobados = [alumno for alumno, prom in promedios.items() if prom >= 6]
+    
+
+    return {
+        "alumno_destacado": [alumno_destacado, (promedio_destacado)],
+        "aprobados": aprobados
+    }
+
+
+curso = {
+    "Ana":     [9, 10, 8, 9, 7],
+    "Luis":    [6,  5, 7, 6, 4],
+    "Sol":     [10, 9, 10, 8, 9],
+    "Marcos":  [4,  5, 3, 6, 2],
+    "Julia":   [7,  8, 7, 9, 8],
+    "Pedro":   [5,  4, 6, 5, 3],
+}
+
+print(f"Reporte del curso: {reporte_curso(curso)}")
+print(f"Resumen del curso: {resumen(curso)}")
