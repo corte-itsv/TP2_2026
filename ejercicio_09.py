553226f22a28aacae1be406744689c47809bfeb2
Octavio Gambino <o.gambino250@itsv.edu.ar>
2026-06-16 20:34:17 +0000

    Resuelve ejercicio 9

---
+++ ejercicio_09.py
@@ -0,0 +1,27 @@
+agenda = {}
+
+def agregar_contacto(agenda, nombre, telefono):
+    agenda[nombre] = telefono
+def buscar_contacto(agenda, nombre):
+    if nombre in agenda:
+        return agenda[nombre]
+    else:
+        return "Contacto no encontrado"
+
+def eliminar_contacto(agenda, nombre):
+    if nombre in agenda: 
+        del agenda[nombre]
+
+def mostrar_agenda(agenda):
+    for nombre in agenda:
+        print(f"{nombre}: {agenda[nombre]}")
+
+agregar_contacto(agenda, "Ana",    "351-1234")
+agregar_contacto(agenda, "Luis",   "351-5678")
+agregar_contacto(agenda, "Marcos", "351-9012")
+
+mostrar_agenda(agenda)
+print(buscar_contacto(agenda, "Luis"))
+print(buscar_contacto(agenda, "Pedro"))
+eliminar_contacto(agenda, "Marcos")     
+mostrar_agenda(agenda)
