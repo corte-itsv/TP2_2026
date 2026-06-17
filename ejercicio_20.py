c1dee963be349900fc05e2deab99244670bc408d
Bautista <b.molina938@itsv.edu.ar>
2026-06-17 00:54:00 -0300

    Se corrige el ejercicio 20

--- ejercicio_20.py
+++ ejercicio_20.py
@@ -1,17 +1,14 @@
 def registrar_alumno(padron, nombre):
     padron.add(nombre)
-
+    
 def votar(votos, ya_votaron, nombre_votante, candidato, padron):    
     if nombre_votante not in padron:
         print(f"{nombre_votante} no está habilitado para votar.")
         return
-
     if nombre_votante in ya_votaron:
         print(f"{nombre_votante} ya votó.")
         return
-
     ya_votaron.add(nombre_votante)
-
     if candidato in ["Ana", "Luis", "Sol"]:
         votos[candidato] += 1
     else:
@@ -22,7 +19,8 @@ def resultado(votos):
     votos_ordenados = dict(sorted(votos.items(), key=lambda item: item[1], reverse=True))
     print("\n=== RESULTADOS ===")
     for candidato, cant in votos_ordenados.items():
-        print(f"{candidato:<6} : {cant} votos")
+        voto_str = "voto" if cant == 1 else "votos"
+        print(f"{candidato:<6} : {cant} {voto_str}")
     return votos_ordenados
 
 def ganador(votos):
@@ -32,26 +30,24 @@ def ganador(votos):
         if candidato != "Blanco" and cantidad > votos_ganador:
             votos_ganador = cantidad
             ganador_actual = candidato
-    return ganador_actual, votos_ganador
+    return ganador_actual
 
 padron = set()
 ya_votaron = set()
 votos = {"Ana": 0, "Luis": 0, "Sol": 0, "Blanco": 0}
-
 registrar_alumno(padron, "Valentina")
 registrar_alumno(padron, "Tomás")
 registrar_alumno(padron, "Camila")
 registrar_alumno(padron, "Diego")
 registrar_alumno(padron, "Lucía")
-
 votar(votos, ya_votaron, "Valentina", "Ana",  padron)
 votar(votos, ya_votaron, "Tomás",     "Sol",  padron)
 votar(votos, ya_votaron, "Camila",    "Ana",  padron)
 votar(votos, ya_votaron, "Diego",     "Luis", padron)
 votar(votos, ya_votaron, "Valentina", "Sol",  padron)
 votar(votos, ya_votaron, "Pedro",     "Ana",  padron)
-votar(votos, ya_votaron, "Lucía",     "Marta", padron)
-
+votar(votos, ya_votaron, "Lucía",     "Marta",padron)
 resultado(votos)
-nom_ganador, cant_votos = ganador(votos)
-print(f"\n🏆 Ganador/a: {nom_ganador} con {cant_votos} votos.")
+nom_ganador = ganador(votos)
+voto_ganador_str = "voto" if votos[nom_ganador] == 1 else "votos"
+print(f"\n🏆 Ganador/a: {nom_ganador} con {votos[nom_ganador]} {voto_ganador_str}.")
