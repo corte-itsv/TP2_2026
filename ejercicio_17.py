a11edb375d5ac9b5b16ae0ab7d5ae5e1f0f3a0db
Bautista <b.molina938@itsv.edu.ar>
2026-06-17 00:53:14 -0300

    Se corrige el ejercicio 17

--- ejercicio_17.py
+++ ejercicio_17.py
@@ -1,20 +1,26 @@
 def cifrar(texto, desplazamiento):
-    texto_cifrado = "" 
-    for caracter in texto:
-        caracter_cif = chr(ord(caracter) + desplazamiento)    
-        texto_cifrado += caracter_cif
+    texto_cifrado = ""
+    for letra in texto:
+        if letra.islower():    
+            caracter_cif = chr((ord(letra) - ord('a') + desplazamiento) % 26 + ord('a'))
+            texto_cifrado += caracter_cif
+        else:
+            texto_cifrado += letra
     return texto_cifrado
 
 def descifrar(texto_cifrado, desplazamiento):
-    texto_descifrado = ""  
-    for caracter in texto_cifrado:
-        caracter_descif = chr(ord(caracter) - desplazamiento)  
-        texto_descifrado += caracter_descif  
+    texto_descifrado = ""
+    for letra in texto_cifrado:
+        if letra.islower():
+            caracter_descif = chr((ord(letra) - ord('a') - desplazamiento) % 26 + ord('a'))
+            texto_descifrado += caracter_descif
+        else:
+            texto_descifrado += letra
     return texto_descifrado
 
 mensaje = "hola mundo"
-clave   = 3
+clave = 3
 mensaje_encriptado = cifrar(mensaje, clave)
-print(f"Texto original: {mensaje}")
-print(f"Texto cifrado: {mensaje_encriptado}")
-print(f"Texto descifrado: {descifrar(mensaje_encriptado, clave)}")
+print(f"Original:  {mensaje}")
+print(f"Cifrado:   {mensaje_encriptado}")
+print(f"Descifrado: {descifrar(mensaje_encriptado, clave)}")
