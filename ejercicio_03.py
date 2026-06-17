5337f47663183dc13c4b8cb59bbcd12b53c3c76e
Bautista <b.molina938@itsv.edu.ar>
2026-06-17 00:41:14 -0300

    Se corrige el ejercicio 3

--- ejercicio_03.py
+++ ejercicio_03.py
@@ -1,9 +1,9 @@
 def contar_aprobados(notas):
     aprobados = 0
     for nota in notas:
-        if nota >=6:
+        if nota >= 6:    
             aprobados = aprobados + 1 
-            return aprobados
+    return aprobados
 
 def contar_desaprobados(notas):
     desaprobados = 0
@@ -12,10 +12,9 @@ def contar_desaprobados(notas):
             desaprobados = desaprobados + 1
     return desaprobados
 
-
 notas = [8, 3, 6, 10, 4, 7, 5, 9, 6, 2]
 cant_aprobados = contar_aprobados(notas)
 print(f"Total: {len(notas)} alumnos")
 print(f"Aprobados: {cant_aprobados}")
 cant_desaprobados = contar_desaprobados(notas)
-print(f"Desaprobados: {cant_desaprobados}")          
+print(f"Desaprobados: {cant_desaprobados}")
