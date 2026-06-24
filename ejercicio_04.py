eea422c1f5f60b3457e6da3fa95ac7b6114f9ea3
Benjamin <b.pagliettini937@gmail.com>
2026-06-24 12:53:04 +0000

    EJERCIO N4

--- ejercicio_04.py
+++ ejercicio_04.py
@@ -1,16 +1,16 @@
 def min_max(numeros):
-    minimo = numeros[0]
-    maximo = numeros[0]
+    numero_minimo = numeros[0] # Hipotesis nada mas
+    numero_maximo = numeros[0] # Hipotesis nada mas
     for numero in numeros:
-        if numero < minimo:
-            minimo = numero
-        if numero > maximo:
-            maximo = numero
-    return (minimo, maximo)
+        if numero < numero_minimo:
+            numero_minimo = numero
+        if numero > numero_maximo:
+            numero_maximo = numero
+    return (numero_minimo, numero_maximo)
 
-numeros = [34, 7, 89, 12, 56, 3, 78, 45]
+numeros_ejemplo = [34, 7, 89, 12, 56, 3, 78, 45]
 
-minimo, maximo = min_max(numeros)
-
-print("Mínimo:", minimo)
-print("Máximo:", maximo)
+numeros_finales = min_max(numeros_ejemplo)
+numero_minimo, numero_maximo = numeros_finales
+print("Mínimo: ", numero_minimo)
+print("Mínimo: ", numero_maximo)
