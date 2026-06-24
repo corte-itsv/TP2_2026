539278856ddd2c55859e366a86f1c3519142990e
Benjamin <b.pagliettini937@gmail.com>
2026-06-24 12:53:21 +0000

    EJERCIO N6

--- ejercicio_06.py
+++ ejercicio_06.py
@@ -1,14 +1,17 @@
 def invertir(lista):
-    nueva = []
-
-    for i in range(len(lista) - 1, -1, -1):
-        nueva.append(lista[i])
-
-    return nueva
+    elementos_invertidos = []
+    for elemento in lista:
+        elementos_invertidos.insert(0, elemento)
+    return elementos_invertidos
 
 
 original = [1, 2, 3, 4, 5]
-letras = ["a", "b", "c", "d"]
+letras   = ["a", "b", "c", "d"]
+animales = ["perro", "gato", "loro"]
 
-print(invertir(original))
-print(invertir(letras))
+originales_invertidos = invertir(original)
+print("Original = ", originales_invertidos)
+letras_invertidas = invertir(letras)
+print("Letras = ", letras_invertidas)
+animales_inertidos = invertir(animales)
+print("Animales = ", animales_inertidos)
