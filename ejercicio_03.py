01a51f7b02a2e3a4c50929801d949e21530af13b
Benjamin <b.pagliettini937@gmail.com>
2026-06-24 12:52:12 +0000

    EJERCICIO N3

--- ejercicio_03.py
+++ ejercicio_03.py
@@ -1,19 +1,40 @@
 def contar_aprobados(notas):
-    aprobados = 0
+    cantidad_num_mayor_o_igual_6 = 0
     for nota in notas:
         if nota >= 6:
-            aprobados += 1
-    return aprobados
+            cantidad_num_mayor_o_igual_6 = cantidad_num_mayor_o_igual_6 + 1 
+    return cantidad_num_mayor_o_igual_6
 
 def contar_desaprobados(notas):
-    desaprobados = 0
+    cantidad_num_menores_6 = 0
     for nota in notas:
         if nota < 6:
-            desaprobados += 1
-    return desaprobados
+            cantidad_num_menores_6 = cantidad_num_menores_6 + 1
+    return cantidad_num_menores_6
 
-notas = [8, 3, 6, 10, 4, 7, 5, 9, 6, 2]
 
-print("Total:", len(notas), "alumnos")
-print("Aprobados:", contar_aprobados(notas))
-print("Desaprobados:", contar_desaprobados(notas))
+notas_benja = [8, 3, 6, 10, 4, 7, 5, 9, 6, 2]
+
+total_de_alumnos = len(notas_benja) 
+
+print("Total: ", total_de_alumnos, "alumnos")
+cant_notas_aprovadas_benja = contar_aprobados(notas_benja)
+print("Aprobadas: ", cant_notas_aprovadas_benja)
+
+cant_notas_desaprobadas_benja = contar_desaprobados(notas_benja)
+print("Desaprobadas: ", cant_notas_desaprobadas_benja)
+
+
+
+
+def saldo_creditos(personas):
+    creditos_totales = 10
+    for persona in personas:
+        if persona == "Lucas":
+            creditos_totales = creditos_totales - 1
+    return creditos_totales
+
+lista_de_nombre = ["Lucas", "Benja", "Tomi", "Lucas"]
+
+saldo_final = saldo_creditos(lista_de_nombre)
+print("Saldo actualizado: ", saldo_final)
