d42f1f72af1d4c2a4880be3d5f9fef7c1279c765
Benjamin <b.pagliettini937@gmail.com>
2026-06-24 12:54:13 +0000

    EJERCIO N11

--- ejercicio_11.py
+++ ejercicio_11.py
@@ -1,13 +1,30 @@
-def sin_duplicados(lista):
-    vistos = set()
-    resultado=[]
-    for elemento in lista:
-        if elemento not in vistos:
-            resultado.append(elemento)
-            vistos.add(elemento)
-    return resultado
+#hacer una funcio "existe_elemento(array, elemento_a_buscar)"
+# que busque un elemento y diga si existe
+# (True) o si no existe (False)
+
+def existe_elemento(array, elemento_a_buscar):
+    for elemento in array:
+        if elemento_a_buscar == elemento:
+            return True
+    return False
+
+def sin_duplicados(lista_original): # 4 billones
+    set_agregados = set()
+    lista_sin_duplicados = []
+    for elemento in lista_original:
+        if elemento not in set_agregados:
+            lista_sin_duplicados.append(elemento)
+            set_agregados.add(elemento)
+    return lista_sin_duplicados
+    
 
 lista = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 nombres = ["Ana", "Luis", "Ana", "Sol", "Luis", "Marcos"]
-print(sin_duplicados(lista))
-print(sin_duplicados(nombres))
+
+respuesta = existe_elemento(lista, "Ana")
+
+
+lista_final_n1 = sin_duplicados(lista)
+print(lista_final_n1)
+lista_final_n2 = sin_duplicados(nombres)
+print(lista_final_n2)
--- ejercicio_12.py
+++ ejercicio_12.py
@@ -1,17 +1,44 @@
 def interseccion(lista_a, lista_b):
-    dos = set(lista_a) & set(lista_b)
-    return sorted(dos)
-    
-def union(lista_a, lista_b):
-    unionf = set(lista_a) | set(lista_b)
-    return sorted(unionf)
+    set_lista = set()
+    lista_final = []
+    for elemento_lista_a in lista_a:
+        for elemento_lista_b in lista_b:
+            if elemento_lista_a == elemento_lista_b and elemento_lista_a not in set_lista:
+                set_lista.add(elemento_lista_a)
+                lista_final.append(elemento_lista_a)
+        
+    return lista_final
 
+def union(lista_a, lista_b):
+    set_lista = set()
+    lista_final = []
+    for elemento_lista_a in lista_a:
+        if elemento_lista_a not in set_lista:
+            set_lista.add(elemento_lista_a)
+            lista_final.append(elemento_lista_a)
+    for elemento_lista_b in lista_b:
+        if elemento_lista_b not in set_lista:
+            set_lista.add(elemento_lista_b)
+            lista_final.append(elemento_lista_b)
+    return lista_final
+    
 def solo_en_a(lista_a, lista_b):
-    dif = set(lista_a) - set(lista_b)
-    return sorted(dif)
+    set_lista_b = set(lista_b)
+    lista_final = []
+    for elemento_lista_a in lista_a:
+        if elemento_lista_a not in set_lista_b:
+            lista_final.append(elemento_lista_a)
+    return lista_final
+    
+    
 clase_lunes    = ["Ana", "Luis", "Sol", "Marcos", "Julia"]
 clase_miercoles = ["Ana", "Sol", "Pedro", "Julia", "Tomás"]
 
-print(f"Asistieron ambos días: {interseccion(clase_lunes, clase_miercoles)}")
-print(f"Asistieron al menos un día: {union(clase_lunes, clase_miercoles)}")
-print(f"Solo el lunes: {solo_en_a(clase_lunes, clase_miercoles)}")
+lista_interseccionada = interseccion(clase_lunes, clase_miercoles)
+print("Asistieron ambos días: ", lista_interseccionada)
+
+lista_unida = union(clase_lunes, clase_miercoles)
+print("Asistieron al menos un día: ", lista_unida)
+
+contenido_de_lista_a = solo_en_a(clase_lunes, clase_miercoles)
+print("Solo el lunes: ", contenido_de_lista_a)
