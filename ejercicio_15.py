1728ac7474069669a5f6c47570c181c9cbb67a60
Benjamin <b.pagliettini937@gmail.com>
2026-06-24 12:59:25 +0000

    EJERCIO N15

--- ejercicio_15.py
+++ ejercicio_15.py
@@ -1,19 +1,24 @@
 def agrupar_por_inicial(nombres):
-    iniciales = {}
-    for nombre in nombres:
-        if nombre[0] not in iniciales:
-            iniciales[nombre[0]] = []
-            iniciales[nombre[0]].append(nombre)
-        else: iniciales[nombre[0]].append(nombre)
-    return iniciales
+    diccionario = {}
+    for nombre_prim_for in nombres:
+        primera_letra_nombre_prim_for = nombre_prim_for[0].upper()
+        diccionario[primera_letra_nombre_prim_for] = [nombre_prim_for]
+        for nombre_seg_for in nombres:
+            primera_letra_nombre_seg_for = nombre_seg_for[0].upper()
+            if primera_letra_nombre_seg_for == primera_letra_nombre_prim_for and nombre_seg_for not in diccionario[primera_letra_nombre_prim_for]:
+                diccionario[primera_letra_nombre_prim_for].append(nombre_seg_for) 
+    return diccionario
 
-nombres = ["Ana", "Alberto", "Belen", "Bruno", "Carlos",
-           "Camila", "Ana Paula", "Diego", "Daniela"]
-resultado_agenda = agrupar_por_inicial(nombres)
-for inicial, lista_de_nombres in resultado_agenda.items():
-    print(f"{inicial}: {lista_de_nombres}") 
-    
-    []
-    {v}
-    {k: v}
-    ()
+def mostrar(diccionario):
+    for primer_letra_nombre, nombres_etc in diccionario.items():
+        print(primer_letra_nombre, ": ", nombres_etc)
+        
+
+
+lista_de_nombres = ["Ana", "Alberto", "Belen", "Bruno", "Carlos", "Camila", "Ana Paula", "Diego", "Daniela"]
+
+
+diccionario_final = agrupar_por_inicial(lista_de_nombres)
+# print(diccionario_final)
+
+mostrar(diccionario_final)
