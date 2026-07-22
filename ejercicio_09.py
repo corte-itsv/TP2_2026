16ed1aaf17fa53045edfc126813930708cd6cc42
Lucas Andino <l.andino568@itsv.edu.ar>
2026-07-22 17:27:43 +0000

    es el ejercicio  09.3

--- ejercicio_09.py
+++ ejercicio_09.py
@@ -1,46 +1,35 @@
 def agregar_contacto(agenda, nombre, telefono):
-    if nombre in agenda:
-        return "El contacto ya existe"
-    else:
-        agenda[nombre] = telefono
-        return "Contacto agregado"
-    
+    agenda[nombre] = telefono
 
 def buscar_contacto(agenda, nombre):
     if nombre in agenda:
-        return f"{agenda[nombre]}"
-    else: 
-        return f"Contacto no encontrado"
-    
-
-def eliminar_contacto(agenda, nombre): 
-    if nombre in agenda: 
-        del agenda[nombre] 
-        return "Contacto eliminado" 
-    else: 
+        return agenda[nombre]
+    else:
         return "Contacto no encontrado"
-    
 
-def mostrar_agenda(agenda): 
-    if not agenda: 
-        return "La agenda está vacía" 
-    contactos = [f"{nombre}: {telefono}" for nombre, telefono in sorted(agenda.items(),
-    key=lambda it: it[0].lower())]
-    return "\n".join(contactos)
+def eliminar_contacto(agenda, nombre):
+    if nombre in agenda:
+        del agenda[nombre]
+
+def mostrar_agenda(agenda):
+    print("=== AGENDA ===")
+    for nombre in sorted(agenda.keys()):
+        print(f"{nombre}: {agenda[nombre]}")
 
 
 agenda = {}
-agregar_contacto(agenda, "Ana",    "351-1234")
-agregar_contacto(agenda, "Luis",   "351-5678")
+agregar_contacto(agenda, "Ana", "351-1234")
+agregar_contacto(agenda, "Luis", "351-5678")
 agregar_contacto(agenda, "Marcos", "351-9012")
 
+mostrar_agenda(agenda)
 
-print("=== AGENDA ===")
-print(mostrar_agenda(agenda))
-print("")
-print("Teléfono de Luis:",buscar_contacto(agenda, "Luis"))
-print("Pedro:",buscar_contacto(agenda, "Pedro"))
-print("")
-print("=== AGENDA ===")
+tel_luis = buscar_contacto(agenda, "Luis")
+print(f"Teléfono de Luis: {tel_luis}")
+
+tel_pedro = buscar_contacto(agenda, "Pedro")
+print(f"Pedro: {tel_pedro}")
+
 eliminar_contacto(agenda, "Marcos")
-print(mostrar_agenda(agenda))
+
+mostrar_agenda(agenda)
