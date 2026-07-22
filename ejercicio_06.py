8a334dfdd5a2a2da3b745a4e1ab2a418ded23195
Octavio Gambino <o.gambino250@itsv.edu.ar>
2026-07-22 17:22:08 +0000

    Correccion ejercicio 06

--- ejercicio_06.py
+++ ejercicio_06.py
@@ -4,7 +4,6 @@ def invertir(lista):
         lista_invertida.append(lista[i])
     return lista_invertida
 original = [1, 2, 3, 4, 5]
+letras   = ["a", "b", "c", "d"]
 print(invertir(original))
-
-        
-        
+print(invertir(letras))
