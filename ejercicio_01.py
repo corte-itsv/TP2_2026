b87e02dbd062fcb317cc2496f9a348e562eaa4c0
smaidana181-blip <s.maidana181@itsv.edu.ar>
2026-07-22 14:32:03 -0300

    Update ejercicio_01.py

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -10,7 +10,7 @@ def clasificar_nota(nota):
     elif nota == 1 or nota == 2 or nota == 3:
         return "Desaprobado (lejos)"
     else:
-        return "Nota no valida"
+        return "Nota Invalida"
 
 
 
